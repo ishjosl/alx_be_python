01e93c2160dd809329b50e612f774d413a5aaf79
Joslin <ishijoslin@gmail.com>
2024-09-26 12:51:20 -0500

    Add weather recommendation script

---
+++ weather_advice.py
@@ -0,0 +1,15 @@
+weather = ["sunny", "rainy", "cold"]
+weather = input("what's the weather outside? (sunny/rainy/cold):").lower()
+if weather == "sunny":
+    recommendation = "wear T-shirt and sunglasses."
+if weather == "rainy":
+    recommendation = "Don't forget your umbrella and a raincoat."
+if weather == "cold":
+    recommendation = "Make sure to wear a warm coat and a scarf."
+
+else:
+    recommendation = "Sorry, I don't have recommendations for this weather."
+
+print(recommendation)
+
+
