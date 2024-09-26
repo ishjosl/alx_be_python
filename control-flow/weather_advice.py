4ac774267a280322e09af01786afed0553545718
Joslin <ishijoslin@gmail.com>
2024-09-26 13:09:58 -0500

    created a weather advice file

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,14 @@
+weather = ["sunny", "rainy", "cold"]
+weather = input("what's the weather outside? (sunny/rainy/cold):").lower()
+if weather == "sunny":
+    recommendation = "wear T-shirt and sunglasses."
+elif weather == "rainy":
+    recommendation = "Don't forget your umbrella and a raincoat."
+elif weather == "cold":
+    recommendation = "Make sure to wear a warm coat and a scarf."
+
+else:
+    recommendation = "Sorry, I don't have recommendations for this weather."
+
+print(recommendation)
+
