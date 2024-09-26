8e22cede2598142655882f5b03ce94a9a01e9b58
Joslin <ishijoslin@gmail.com>
2024-09-26 12:56:09 -0500

    updated the script

--- weather_advice.py
+++ weather_advice.py
@@ -2,9 +2,9 @@ weather = ["sunny", "rainy", "cold"]
 weather = input("what's the weather outside? (sunny/rainy/cold):").lower()
 if weather == "sunny":
     recommendation = "wear T-shirt and sunglasses."
-if weather == "rainy":
+elif weather == "rainy":
     recommendation = "Don't forget your umbrella and a raincoat."
-if weather == "cold":
+elif weather == "cold":
     recommendation = "Make sure to wear a warm coat and a scarf."
 
 else:
