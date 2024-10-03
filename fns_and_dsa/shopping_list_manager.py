478a29a310667e414bb42381244baad03d558f7e
Joslin <ishijoslin@gmail.com>
2024-10-03 12:35:10 -0500

    correction

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,13 +10,13 @@ def display_menu():
         display_menu()
         choice= input("Enter your choice:")
 
-        if choice =='1':
+        if choice == '1':
             pass
-        elif choice =='2':
+        elif choice == '2':
             pass
-        elif choice =='3':
+        elif choice == '3':
             pass
-        elif choice =='4':
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
