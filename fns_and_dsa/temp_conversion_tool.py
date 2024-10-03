364b512eadade7379051d1e510e5ab38f34626be
Joslin <ishijoslin@gmail.com>
2024-10-03 13:02:52 -0500

    new version

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,3 @@
-# temp_conversion_tool.py
-
-# Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 FAHRENHEIT_OFFSET = 32
@@ -30,5 +27,3 @@ def main():
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
 
-if __name__ == "__main__":
-    main()
