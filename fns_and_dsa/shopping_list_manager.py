67bd5f86ac3dbaa4529be2ac1c561b7aced0cbef
Joslin <ishijoslin@gmail.com>
2024-10-03 12:32:21 -0500

    shopping list manager file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,28 @@
+def display_menu():
+    shopping_list= []
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+    while True:
+        display_menu()
+        choice= input("Enter your choice:")
+
+        if choice ==1:
+            pass
+        elif choice ==2:
+            pass
+        elif choice ==3:
+            pass
+        elif choice ==4:
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again:")
+            
+
+     
+        
+       
