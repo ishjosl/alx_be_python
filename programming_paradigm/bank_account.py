320f739982be420a1603662cb1b7dd0779f639f4
Joslin ISHIMWE <joslinishimwe@Joslins-Air.home>
2024-10-09 18:37:45 -0500

    updates

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,17 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
+    def __init__(self, initial_balance = 0):
         self._account_balance = initial_balance
 
     def deposit(self, amount):
         if amount > 0:
             self._account_balance += account
+            return True
+        return False
     def withdraw(self, amount):
-        if amount > 0 and amount < self. _account_balance:
+        if amount > 0 and amount <= self. _account_balance:
             self._account_balance -= account
-        else:
-            return False        
+            return True
+        return False       
     def display_balance(self):
         print(f"Current balance: ${self._account_balance:2f}")
 
