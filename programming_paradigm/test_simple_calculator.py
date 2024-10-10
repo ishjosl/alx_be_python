90052fc7a507ca3bbde47268b83cefd5800dcb3d
Joslin ISHIMWE <joslinishimwe@Joslins-Air.home>
2024-10-09 19:31:54 -0500

    tested mult and divide

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,6 +11,17 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        # Add more assertions to thoroughly test the add method.
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2,4), 10)
+        self.assertEqual(self.calc.subtract(3,4), 0)
+    
+    def test_muliply(self):
+        self.assertEqual(self.calc.multiply(4,5), 9)
+        self.assertEqual(self.calc.multiply(0,1), 10)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4,6), 10)
+        self.assertEqual(self.calc.divide(4, 1), 0)
 
 # Remember to write additional test methods for subtract, multiply, and divide.
